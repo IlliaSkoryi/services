2d72eece6f7e1711817160704c73cf7053d086c3
Illia Skoryi <skorillia@gmail.com>
2019-11-25 20:19:19 +0100

    service discovery provided

--- news-service/src/main/java/com/services/newsservice/NewsServiceApplication.java
+++ news-service/src/main/java/com/services/newsservice/NewsServiceApplication.java
@@ -2,10 +2,12 @@ package com.services.newsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @RefreshScope
+@EnableDiscoveryClient
 public class NewsServiceApplication {
 
 	public static void main(String[] args) {
--- users-service/src/main/java/com/services/usersservice/UsersServiceApplication.java
+++ users-service/src/main/java/com/services/usersservice/UsersServiceApplication.java
@@ -2,10 +2,12 @@ package com.services.usersservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @RefreshScope
+@EnableDiscoveryClient
 public class UsersServiceApplication {
 
 	public static void main(String[] args) {
