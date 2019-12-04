e0597c52554906e57da9e3ffa43e1e6f06c41188
Illia Skoryi <skorillia@gmail.com>
2019-12-04 17:36:08 +0200

    checkstyle for discovery-service adjusted, code clean-up completed.

--- discovery/src/main/java/com/services/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/com/services/discovery/DiscoveryApplication.java
@@ -4,10 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * Discovery application service starter class.
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryApplication {
 
+    /**
+     * Entry point.
+     *
+     * @param args parameters arguments
+     */
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryApplication.class, args);
     }
